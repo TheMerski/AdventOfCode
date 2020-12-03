b8ef7a1f68007d69647f5750d53c6460c03e5290
Merwin Hill <merwin@quicknet.nl>
2020-12-03 07:54:38 +0100

    Completed day 3

---
+++ AOC/AOC2020/Day3.cs
@@ -0,0 +1,44 @@
+﻿using AOC2020.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AOC2020
+{
+    class Day3
+    {
+        private TimeHelper timeHelper;
+        private string[] input;
+        public Day3()
+        {
+            timeHelper = new TimeHelper();
+            input = ResourceReader.ReadResourceToStringArray(Properties.Resources.Day3, Environment.NewLine);
+            timeHelper.Start();
+            int slope1 = FindTrees(1, 1, '#');
+            int slope2 = FindTrees(3, 1, '#');
+            int slope3 = FindTrees(5, 1, '#');
+            int slope4 = FindTrees(7, 1, '#');
+            int slope5 = FindTrees(1, 2, '#');
+            long result = (long)slope1 * (long)slope2 * (long)slope3 * (long)slope4 * (long)slope5;
+            Console.WriteLine(result);
+            timeHelper.Stop();
+        }
+
+        private int FindTrees(int right, int down, char tree)
+        {
+            int trees = 0;
+            int x = 0;
+            for (int y = 0; y < input.Length; y += down)
+            {
+                string row = input[y];
+                if (row[x % row.Length] == tree)
+                {
+                    trees++;
+                }
+                x += right;
+            }
+            return trees;
+        }
+    }
+}
--- AOC/AOC2020/Program.cs
+++ AOC/AOC2020/Program.cs
@@ -10,7 +10,7 @@ namespace AOC2020
 
         static void Main(string[] args)
         {
-            new Day2();
+            new Day3();
         }
 
 
